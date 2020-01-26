31f4955b88545750c3de6dc197693b9c319076c7
Alex Paschenko <past.first@gmail.com>
2020-01-26 23:27:05 +0200

    Continue to implement the parser

--- 2.js
+++ 2.js
@@ -1,16 +1,10 @@
 
-class Test {
-    constructor(aaa) {
-        this.bbb = aaa;
-    }
-}
+const t = require('./index').render;
 
-const test1 = new Test('test1');
+const data = {
+    start: ' as {{ tt }} qwerty ',
+    tt: 123
+};
 
-console.log('1.1: ', test1.bbb);
 
-const test2 = new Test('test2');
-
-console.log('2: ', test2.bbb);
-
-console.log('1.2: ', test1.bbb);
+console.log(t(data));
