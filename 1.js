9524a559e5fbf1807cc2a3c7fc721ffa6a2e163b
Alexander Paschenko <apaschenko@3knbh.com>
2020-02-24 19:43:16 +0200

    Qualified names: finish to debug of error printing

--- 1.js
+++ 1.js
@@ -3,7 +3,7 @@
 async function aaa () {
     let ii =    '0         10        20        30        40        50        60        70        80        90        100       110       120       130       140   ';
     let index = '01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345';
-    let start = 'q # {{# comment}} {{unless bbb}}bbb is missing.(( = bbb.ccc ddd.eee.fff( ttt.kkk.mmm, zzz.$$$, wer.der) )){{end}}{{ if tester}} tester is {{aaa}} true tester {{else}}if false {{end}} {{-q-w-e-# asset';
+    let start = 'q # {{# comment}} {{unless bbb}}bbb is missing.(( = bbb.ccc ddd.eee.fff( ttt.kkk.mmm, zzz.$$$, wer.der) )){{end}}{{ if tester}} tester is {{aaa}} true tester {{else}}if false {{end}} {{-q-w-e-# asset}}w';
 console.log('abc'.toString())
     const res = await require('./index')
         .render(
